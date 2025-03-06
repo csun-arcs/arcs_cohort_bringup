9f5c8f8d7c8d4ee42a63aa8bfebb50d9d5fde9c6
Barry Ridge <barry.ridge@csun.edu>
2025-03-06 11:50:12 -0800

    Add robot_name & namespace args & RViz config gen

--- launch/simulation_bringup.launch.py
+++ launch/simulation_bringup.launch.py
@@ -2,7 +2,7 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, ExecuteProcess
 from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import (
@@ -11,7 +11,7 @@ from launch.substitutions import (
     PathJoinSubstitution,
     PythonExpression
 )
-from launch_ros.actions import Node
+from launch_ros.actions import Node, PushRosNamespace
 from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
@@ -27,7 +27,12 @@ def generate_launch_description():
         'empty.world'
     )
     default_model_path = 'description/robot.urdf.xacro'
-    default_rviz_config_path = os.path.join(
+    default_rviz_config_template_file = os.path.join(
+        get_package_share_directory(pkg_description),
+        "rviz_config",
+        "robot_model.rviz.template"
+    )
+    default_rviz_config_file = os.path.join(
         get_package_share_directory(pkg_description),
         'rviz_config',
         'robot_model.rviz'
@@ -54,6 +59,16 @@ def generate_launch_description():
         default_value=default_model_path,
         description='Relative path to the robot model file'
     )
+    declare_robot_name_cmd = DeclareLaunchArgument(
+        'robot_name',
+        default_value='',
+        description='Name of the robot'
+    )
+    declare_namespace_cmd = DeclareLaunchArgument(
+        "namespace",
+        default_value="",
+        description="Namespace under which to bring up nodes, topics, etc."
+    )
     declare_use_rsp_cmd = DeclareLaunchArgument(
         'use_rsp',
         default_value='true',
@@ -74,9 +89,18 @@ def generate_launch_description():
         default_value='true',
         description='Launch RViz'
     )
+    declare_use_rviz_config_template_cmd = DeclareLaunchArgument(
+        "use_rviz_config_template", default_value="true",
+        description="If true, generate the RViz config from the specified RViz config template."
+    )
+    declare_rviz_config_template_cmd = DeclareLaunchArgument(
+        "rviz_config_template",
+        default_value=default_rviz_config_template_file,
+        description="Path to the RViz config template file",
+    )
     declare_rviz_config_cmd = DeclareLaunchArgument(
         'rviz_config',
-        default_value=default_rviz_config_path,
+        default_value=default_rviz_config_file,
         description='Path to RViz configuration file'
     )
 
@@ -85,49 +109,50 @@ def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time')
     model_package = LaunchConfiguration('model_package')
     model_file = LaunchConfiguration('model_file')
+    robot_name = LaunchConfiguration('robot_name')
+    namespace = LaunchConfiguration("namespace")
     use_rsp = LaunchConfiguration('use_rsp')
     use_jsp = LaunchConfiguration('use_jsp')
     use_jsp_gui = LaunchConfiguration('use_jsp_gui')
     use_rviz = LaunchConfiguration('use_rviz')
+    use_rviz_config_template = LaunchConfiguration("use_rviz_config_template")
+    rviz_config_template = LaunchConfiguration("rviz_config_template")
     rviz_config = LaunchConfiguration('rviz_config')
 
-    # Include the gazebo_sim.launch.py
-    gazebo_sim_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([
-            os.path.join(
-                get_package_share_directory('arcs_cohort_gazebo_sim'),
-                'launch',
-                'gazebo_sim.launch.py'
-            )
-        ]),
-        launch_arguments={
-            'world': world,
-            'use_sim_time': use_sim_time,
-            'model_package': model_package,
-            'model_file': model_file,
-            'use_rsp': 'false',  # Disable RSP in gazebo_sim
-            'use_jsp': 'false',  # Disable JSP in gazebo_sim
-            'use_jsp_gui': 'false',  # Disable JSP GUI in gazebo_sim
-        }.items()
+    # Compute the robot prefix only if a robot name is provided
+    # This expression will evaluate to, for example, "cohort_" if
+    # robot_name is "cohort", or to an empty string if robot_name is empty.
+    robot_prefix = PythonExpression(
+        ["'", robot_name, "_' if '", robot_name, "' else ''"]
+    )
+    # Compute the prefix argument only if a robot_name/robot_prefix is provided.
+    # This expression will evaluate to, for example, "prefix:=cohort_" if
+    # robot_prefix is "cohort_", or to an empty string if robot_prefix is empty.
+    robot_prefix_arg = PythonExpression(
+        ["('prefix:=' + '", robot_prefix, "') if '", robot_prefix, "' else ''"]
     )
 
-    # Robot description
-    robot_description_content = Command([
-        'xacro ',
-        PathJoinSubstitution([
-            FindPackageShare(model_package),
-            model_file
-        ]),
-        ' use_sim_time:=', use_sim_time
-    ])
-    robot_description = {'robot_description': robot_description_content}
+    # Robot description from Xacro, including the conditional robot name prefix.
+    robot_description = Command(
+        [
+            "xacro ",
+            PathJoinSubstitution([FindPackageShare(model_package), model_file]),
+            " ",
+            robot_prefix_arg,
+        ]
+    )
+
+    # Use PushRosNamespace to apply the namespace to all nodes below
+    push_namespace = PushRosNamespace(namespace)
 
     # Robot State Publisher node
     rsp_node = Node(
         condition=IfCondition(use_rsp),
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        parameters=[robot_description, {'use_sim_time': use_sim_time}],
+        parameters=[
+            {"robot_description": robot_description, "use_sim_time": use_sim_time}
+        ],
         output='screen'
     )
 
@@ -155,6 +180,29 @@ def generate_launch_description():
         output='screen'
     )
 
+    # Generate RViz config from template.
+    # The robot prefix will be substituted into the RViz config template in
+    # place of the ARCS_COHORT_PREFIX variable and the namespace will be
+    # substituted in place of ARCS_COHORT_NAMESPACE.
+    #
+    # NOTE: We should probably change this approach later.  It's a neat trick,
+    # but might not be manageable/scaleable.  Using fixed RViz configurations
+    # for different robot/world scenarios is probably a more robust approach.
+    # This type of dynamic RViz config generation could still be useful in the
+    # early stages of project development to test namespacing, prefixing, etc.
+    #
+    namespace_env_var = PythonExpression(
+        ["'/", namespace, "' if '", namespace, "' else ''"]
+    )
+    rviz_config_generator = ExecuteProcess(
+        condition=IfCondition(use_rviz_config_template),
+        cmd=[["ARCS_COHORT_PREFIX='", robot_prefix, "' ",
+              "ARCS_COHORT_NAMESPACE='", namespace_env_var, "' ",
+              "envsubst < ", rviz_config_template, " > ", rviz_config]],
+        shell=True,
+        output='screen',
+    )
+
     # RViz node
     rviz_node = Node(
         condition=IfCondition(use_rviz),
@@ -165,21 +213,47 @@ def generate_launch_description():
         parameters=[{'use_sim_time': use_sim_time}]
     )
 
+    # Include the gazebo_sim.launch.py
+    gazebo_sim_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            os.path.join(
+                get_package_share_directory('arcs_cohort_gazebo_sim'),
+                'launch',
+                'gazebo_sim.launch.py'
+            )
+        ]),
+        launch_arguments={
+            'world': world,
+            'use_sim_time': use_sim_time,
+            'model_package': model_package,
+            'model_file': model_file,
+            'use_rsp': 'false',  # Disable RSP in gazebo_sim
+            'use_jsp': 'false',  # Disable JSP in gazebo_sim
+            'use_jsp_gui': 'false',  # Disable JSP GUI in gazebo_sim
+        }.items()
+    )
+
     return LaunchDescription([
         # Declare launch arguments
         declare_world_cmd,
         declare_use_sim_time_cmd,
         declare_model_package_cmd,
         declare_model_file_cmd,
+        declare_robot_name_cmd,
+        declare_namespace_cmd,
         declare_use_rsp_cmd,
         declare_use_jsp_cmd,
         declare_use_jsp_gui_cmd,
         declare_use_rviz_cmd,
+        declare_use_rviz_config_template_cmd,
+        declare_rviz_config_template_cmd,
         declare_rviz_config_cmd,
         # Nodes
+        push_namespace,
         rsp_node,
         jsp_node,
         jsp_gui_node,
+        rviz_config_generator,
         rviz_node,
         # Launchers
         gazebo_sim_launch,
