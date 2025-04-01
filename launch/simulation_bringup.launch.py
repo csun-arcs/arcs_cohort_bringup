19628a5f2c47faa33673f06b034484b7a432a26f
pikaic <pikaic2010@gmail.com>
2025-04-01 14:06:37 -0700

    Add LiDAR launch argument

--- launch/simulation_bringup.launch.py
+++ launch/simulation_bringup.launch.py
@@ -2,106 +2,102 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, ExecuteProcess
+from launch.actions import (
+    DeclareLaunchArgument,
+    IncludeLaunchDescription,
+    ExecuteProcess,
+)
 from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import (
     Command,
     LaunchConfiguration,
     PathJoinSubstitution,
-    PythonExpression
+    PythonExpression,
 )
 from launch_ros.actions import Node, PushRosNamespace
 from launch_ros.substitutions import FindPackageShare
 
+
 def generate_launch_description():
     # Package and file paths
-    pkg_bringup = 'arcs_cohort_bringup'
-    pkg_gazebo_sim = 'arcs_cohort_gazebo_sim'
-    pkg_description = 'arcs_cohort_description'
+    pkg_bringup = "arcs_cohort_bringup"
+    pkg_gazebo_sim = "arcs_cohort_gazebo_sim"
+    pkg_description = "arcs_cohort_description"
 
     # Paths to default files
     default_world_path = os.path.join(
-        get_package_share_directory(pkg_gazebo_sim),
-        'worlds',
-        'empty.world'
+        get_package_share_directory(pkg_gazebo_sim), "worlds", "empty.world"
     )
-    default_model_path = 'description/robot.urdf.xacro'
+    default_model_path = "description/robot.urdf.xacro"
     default_rviz_config_template_file = os.path.join(
         get_package_share_directory(pkg_description),
         "rviz_config",
-        "robot_model.rviz.template"
+        "robot_model.rviz.template",
     )
     default_rviz_config_file = os.path.join(
-        get_package_share_directory(pkg_description),
-        'rviz_config',
-        'robot_model.rviz'
+        get_package_share_directory(pkg_description), "rviz_config", "robot_model.rviz"
     )
 
     # Declare launch arguments
     declare_world_cmd = DeclareLaunchArgument(
-        'world',
+        "world",
         default_value=default_world_path,
-        description='Path to the world file to load'
+        description="Path to the world file to load",
     )
     declare_use_sim_time_cmd = DeclareLaunchArgument(
-        'use_sim_time',
-        default_value='true',
-        description='Use simulation time'
+        "use_sim_time", default_value="true", description="Use simulation time"
     )
     declare_model_package_cmd = DeclareLaunchArgument(
-        'model_package',
+        "model_package",
         default_value=pkg_description,
-        description='Package containing the robot model'
+        description="Package containing the robot model",
     )
     declare_model_file_cmd = DeclareLaunchArgument(
-        'model_file',
+        "model_file",
         default_value=default_model_path,
-        description='Relative path to the robot model file'
+        description="Relative path to the robot model file",
     )
     declare_robot_name_cmd = DeclareLaunchArgument(
-        'robot_name',
-        default_value='',
+        "robot_name",
+        default_value="",
         description=(
-            'Name of the robot (specifying this will add the '
-            'robot name prefix to joints, links, etc. in the robot model).')
+            "Name of the robot (specifying this will add the "
+            "robot name prefix to joints, links, etc. in the robot model)."
+        ),
     )
     declare_camera_resolution_cmd = DeclareLaunchArgument(
-        'camera_resolution',
-        default_value='VGA',
+        "camera_resolution",
+        default_value="VGA",
         description=(
-            'Resolution profile of the simulated Stereolabs Zed camera.'
+            "Resolution profile of the simulated Stereolabs Zed camera."
             'Options: "HD2K" (2208x1242), "HD1080" (1920x1080), '
-            '"HD720" (1280x720) or "VGA" (672x376).')
+            '"HD720" (1280x720) or "VGA" (672x376).'
+        ),
     )
     declare_namespace_cmd = DeclareLaunchArgument(
         "namespace",
         default_value="",
-        description="Namespace under which to bring up nodes, topics, etc."
+        description="Namespace under which to bring up nodes, topics, etc.",
     )
     declare_use_rsp_cmd = DeclareLaunchArgument(
-        'use_rsp',
-        default_value='true',
-        description='Launch robot_state_publisher'
+        "use_rsp", default_value="true", description="Launch robot_state_publisher"
     )
     declare_use_jsp_cmd = DeclareLaunchArgument(
-        'use_jsp',
-        default_value='false',
-        description='Launch joint_state_publisher'
+        "use_jsp", default_value="false", description="Launch joint_state_publisher"
     )
     declare_use_jsp_gui_cmd = DeclareLaunchArgument(
-        'use_jsp_gui',
-        default_value='false',
-        description='Launch joint_state_publisher_gui'
+        "use_jsp_gui",
+        default_value="false",
+        description="Launch joint_state_publisher_gui",
     )
     declare_use_rviz_cmd = DeclareLaunchArgument(
-        'use_rviz',
-        default_value='true',
-        description='Launch RViz'
+        "use_rviz", default_value="true", description="Launch RViz"
     )
     declare_use_rviz_config_template_cmd = DeclareLaunchArgument(
-        "use_rviz_config_template", default_value="true",
-        description="If true, generate the RViz config from the specified RViz config template."
+        "use_rviz_config_template",
+        default_value="true",
+        description="If true, generate the RViz config from the specified RViz config template.",
     )
     declare_rviz_config_template_cmd = DeclareLaunchArgument(
         "rviz_config_template",
@@ -109,26 +105,32 @@ def generate_launch_description():
         description="Path to the RViz config template file",
     )
     declare_rviz_config_cmd = DeclareLaunchArgument(
-        'rviz_config',
+        "rviz_config",
         default_value=default_rviz_config_file,
-        description='Path to RViz configuration file'
+        description="Path to RViz configuration file",
+    )
+    declare_use_lidar_cmd = DeclareLaunchArgument(
+        "use_lidar",
+        default_value="false",
+        description="If true, include the lidar in the robot description",
     )
 
     # Launch configurations
-    world = LaunchConfiguration('world')
-    use_sim_time = LaunchConfiguration('use_sim_time')
-    model_package = LaunchConfiguration('model_package')
-    model_file = LaunchConfiguration('model_file')
-    robot_name = LaunchConfiguration('robot_name')
-    camera_resolution = LaunchConfiguration('camera_resolution')
+    world = LaunchConfiguration("world")
+    use_sim_time = LaunchConfiguration("use_sim_time")
+    model_package = LaunchConfiguration("model_package")
+    model_file = LaunchConfiguration("model_file")
+    robot_name = LaunchConfiguration("robot_name")
+    camera_resolution = LaunchConfiguration("camera_resolution")
     namespace = LaunchConfiguration("namespace")
-    use_rsp = LaunchConfiguration('use_rsp')
-    use_jsp = LaunchConfiguration('use_jsp')
-    use_jsp_gui = LaunchConfiguration('use_jsp_gui')
-    use_rviz = LaunchConfiguration('use_rviz')
+    use_rsp = LaunchConfiguration("use_rsp")
+    use_jsp = LaunchConfiguration("use_jsp")
+    use_jsp_gui = LaunchConfiguration("use_jsp_gui")
+    use_rviz = LaunchConfiguration("use_rviz")
     use_rviz_config_template = LaunchConfiguration("use_rviz_config_template")
     rviz_config_template = LaunchConfiguration("rviz_config_template")
-    rviz_config = LaunchConfiguration('rviz_config')
+    rviz_config = LaunchConfiguration("rviz_config")
+    use_lidar = LaunchConfiguration("use_lidar")
 
     # Compute the robot prefix only if a robot name is provided
     # This expression will evaluate to, for example, "cohort_" if
@@ -153,6 +155,8 @@ def generate_launch_description():
             " ",
             "camera_resolution:=",
             camera_resolution,
+            " use_lidar:=",
+            use_lidar,
         ]
     )
 
@@ -162,12 +166,12 @@ def generate_launch_description():
     # Robot State Publisher node
     rsp_node = Node(
         condition=IfCondition(use_rsp),
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
         parameters=[
             {"robot_description": robot_description, "use_sim_time": use_sim_time}
         ],
-        output='screen'
+        output="screen",
     )
 
     # Joint State Publisher node
@@ -177,21 +181,21 @@ def generate_launch_description():
                 ["'", use_jsp, "' == 'true' and '", use_jsp_gui, "' != 'true'"]
             )
         ),
-        package='joint_state_publisher',
-        executable='joint_state_publisher',
-        name='joint_state_publisher',
-        parameters=[{'use_sim_time': use_sim_time}],
-        output='screen'
+        package="joint_state_publisher",
+        executable="joint_state_publisher",
+        name="joint_state_publisher",
+        parameters=[{"use_sim_time": use_sim_time}],
+        output="screen",
     )
 
     # Joint State Publisher GUI node
     jsp_gui_node = Node(
         condition=IfCondition(use_jsp_gui),
-        package='joint_state_publisher_gui',
-        executable='joint_state_publisher_gui',
-        name='joint_state_publisher_gui',
-        parameters=[{'use_sim_time': use_sim_time}],
-        output='screen'
+        package="joint_state_publisher_gui",
+        executable="joint_state_publisher_gui",
+        name="joint_state_publisher_gui",
+        parameters=[{"use_sim_time": use_sim_time}],
+        output="screen",
     )
 
     # Generate RViz config from template.
@@ -210,66 +214,82 @@ def generate_launch_description():
     )
     rviz_config_generator = ExecuteProcess(
         condition=IfCondition(use_rviz_config_template),
-        cmd=[["ARCS_COHORT_PREFIX='", robot_prefix, "' ",
-              "ARCS_COHORT_NAMESPACE='", namespace_env_var, "' ",
-              "envsubst < ", rviz_config_template, " > ", rviz_config]],
+        cmd=[
+            [
+                "ARCS_COHORT_PREFIX='",
+                robot_prefix,
+                "' ",
+                "ARCS_COHORT_NAMESPACE='",
+                namespace_env_var,
+                "' ",
+                "envsubst < ",
+                rviz_config_template,
+                " > ",
+                rviz_config,
+            ]
+        ],
         shell=True,
-        output='screen',
+        output="screen",
     )
 
     # RViz node
     rviz_node = Node(
         condition=IfCondition(use_rviz),
-        package='rviz2',
-        executable='rviz2',
-        output='screen',
-        arguments=['-d', rviz_config],
-        parameters=[{'use_sim_time': use_sim_time}]
+        package="rviz2",
+        executable="rviz2",
+        output="screen",
+        arguments=["-d", rviz_config],
+        parameters=[{"use_sim_time": use_sim_time}],
     )
 
     # Include the gazebo_sim.launch.py
     gazebo_sim_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([
-            os.path.join(
-                get_package_share_directory('arcs_cohort_gazebo_sim'),
-                'launch',
-                'gazebo_sim.launch.py'
-            )
-        ]),
+        PythonLaunchDescriptionSource(
+            [
+                os.path.join(
+                    get_package_share_directory("arcs_cohort_gazebo_sim"),
+                    "launch",
+                    "gazebo_sim.launch.py",
+                )
+            ]
+        ),
         launch_arguments={
-            'world': world,
-            'use_sim_time': use_sim_time,
-            'model_package': model_package,
-            'model_file': model_file,
-            'use_rsp': 'false',  # Disable RSP in gazebo_sim
-            'use_jsp': 'false',  # Disable JSP in gazebo_sim
-            'use_jsp_gui': 'false',  # Disable JSP GUI in gazebo_sim
-        }.items()
+            "world": world,
+            "use_sim_time": use_sim_time,
+            "model_package": model_package,
+            "model_file": model_file,
+            "use_rsp": "false",  # Disable RSP in gazebo_sim
+            "use_jsp": "false",  # Disable JSP in gazebo_sim
+            "use_jsp_gui": "false",  # Disable JSP GUI in gazebo_sim
+        }.items(),
     )
 
-    return LaunchDescription([
-        # Declare launch arguments
-        declare_world_cmd,
-        declare_use_sim_time_cmd,
-        declare_model_package_cmd,
-        declare_model_file_cmd,
-        declare_robot_name_cmd,
-        declare_camera_resolution_cmd,
-        declare_namespace_cmd,
-        declare_use_rsp_cmd,
-        declare_use_jsp_cmd,
-        declare_use_jsp_gui_cmd,
-        declare_use_rviz_cmd,
-        declare_use_rviz_config_template_cmd,
-        declare_rviz_config_template_cmd,
-        declare_rviz_config_cmd,
-        # Nodes
-        push_namespace,
-        rsp_node,
-        jsp_node,
-        jsp_gui_node,
-        rviz_config_generator,
-        rviz_node,
-        # Launchers
-        gazebo_sim_launch,
-    ])
+    return LaunchDescription(
+        [
+            # Declare launch arguments
+            declare_world_cmd,
+            declare_use_sim_time_cmd,
+            declare_model_package_cmd,
+            declare_model_file_cmd,
+            declare_robot_name_cmd,
+            declare_camera_resolution_cmd,
+            declare_namespace_cmd,
+            declare_use_rsp_cmd,
+            declare_use_jsp_cmd,
+            declare_use_jsp_gui_cmd,
+            declare_use_rviz_cmd,
+            declare_use_rviz_config_template_cmd,
+            declare_rviz_config_template_cmd,
+            declare_rviz_config_cmd,
+            declare_use_lidar_cmd,
+            # Nodes
+            push_namespace,
+            rsp_node,
+            jsp_node,
+            jsp_gui_node,
+            rviz_config_generator,
+            rviz_node,
+            # Launchers
+            gazebo_sim_launch,
+        ]
+    )
